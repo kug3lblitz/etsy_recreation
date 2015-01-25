60d4d519652d380d0721e761c61d627f4a42b0b2
Patrick Werner <pwerner2@gmail.com>
2015-01-25 13:00:56 -0500

    have not successfully incorporated API yet

--- script/main.js
+++ script/main.js
@@ -1,28 +1,64 @@
 (function(){
-  'use strict';
+	'use strict';
 
-  var beers = rawBeerData.beers;
+	var legos = rawLegoData.results;
 
-  $(document).ready(function(){
+	$(document).ready(function(){
 
-    var $list = $('.beers-list');
+		var $output = $('.data');
 
-    beers.forEach(function(beer){
-      var beerText = renderTemplate('beer-item', {
-        name: beer.name,
-        brewery: beer.brewery.name
-      });
-      $list.append(beerText);
-    });
 
-  });
+		$(".sort-dropdown").change(function(sort) {
+			if ($(".sort-dropdown option:selected").text() == "Lowest Price") {
+				legos = _.sortBy(legos, function(lego) {
+					return +lego.price;
+				});
+			} else if ($(".sort-dropdown option:selected").text() == "Highest Price") {
+				legos = _.sortBy(legos, function(lego) {
+					return +lego.price;
+				}).reverse();
+			}
+			changeOrder(legos);
+		});
 
-  function renderTemplate(name, data) {
-    var $template = $('[data-template-name=' + name + ']').text();
-    $.each(data, function(prop, value) {
-      $template = $template.replace('<% ' + prop + ' %>', value);
-    });
-    return $template;
-  }
+		function changeOrder(data) {
+			$output.empty();
+			data.forEach(function(lego) {
+				var legoInfo = renderTemplate('lego-item', {
+					image: lego.Images[0].url_170x135,
+					title: lego.title,
+					titleURL: lego.url,
+					shopName: lego.Shop.shop_name,
+					price: lego.price,
+					currency: lego.currency_code
+				});
+				$output.append(legoInfo);
+			});
+		}
+
+		legos.forEach(function(lego){
+			var legoInfo = renderTemplate('lego-item', {
+				image: lego.Images[0].url_170x135,
+				title: lego.title,
+				titleURL: lego.url,
+				shopName: lego.Shop.shop_name,
+				price: lego.price,
+				currency: lego.currency_code
+			});
+			$output.append(legoInfo);
+		});
+
+	});
+
+	/*
+	Templete function
+	*/
+	function renderTemplate(name, data) {
+		var $template = $('[data-template-name=' + name + ']').text();
+		$.each(data, function(prop, value) {
+			$template = $template.replace('<% ' + prop + ' %>', value);
+		});
+		return $template;
+	}
 
 })();
